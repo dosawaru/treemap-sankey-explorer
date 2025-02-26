c0086581b75eb6e8cbb8f19db0f6298cb90d06b2
Daniel Osawaru <35234154+dosawaru@users.noreply.github.com>
2025-02-26 16:00:05 -0700

    Add input validation, styling to treemap, and redraw functionality

--- dosawaru.js
+++ dosawaru.js
@@ -1,9 +1,11 @@
-// Define margin and dimension for treemap
+// Define  dimension for treemap
+const width = 580;
+const height = 400;
 
-const margin = 10;
-
-const width = 580 - margin;
-const height = 400 - margin;
+// Define the characters that are punctuation, consonants, and vowels
+const consonants = "bcdfghjklmnpqrstvwxz";
+const vowels = "aeiouy";
+const punctuation = ".,!?:;";
 
 let data = {};
 
@@ -12,9 +14,12 @@ document.addEventListener("DOMContentLoaded", function () {
   let charCount = { consonants: {}, vowels: {}, punctuation: {} };
 
   // Create treemap
-  const map = d3.select("#treemap_svg");
+  let map = d3.select("#treemap_svg");
 
   function saveText() {
+    // Clears char count
+    charCount = { consonants: {}, vowels: {}, punctuation: {} };
+
     // Get the text from the textarea
     let text = document.getElementById("wordbox").value;
 
@@ -22,11 +27,6 @@ document.addEventListener("DOMContentLoaded", function () {
     let consonantsCount = 0;
     let vowelsCount = 0;
     let punctuationCount = 0;
-
-    // Define the characters that are punctuation, consonants, and vowels
-    const consonants = "bcdfghjklmnpqrstvwxz";
-    const vowels = "aeiouy";
-    const punctuation = ".,!?:;";
 
     // Loop through each character in the text
     for (let c of text) {
@@ -44,6 +44,13 @@ document.addEventListener("DOMContentLoaded", function () {
         punctuationCount++;
       }
     }
+
+    if (consonantsCount + vowelsCount + punctuationCount == 0) {
+      alert(
+        `Invalid input, please enter in as least one of the following: \nconsonants -> ${consonants} \nvowels -> ${vowels} \npunctuation -> ${punctuation} \n😊`
+      );
+      return;
+    }
     console.log(charCount);
     console.log(consonantsCount);
     console.log(vowelsCount);
@@ -54,31 +61,51 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function drawTreemap() {
+    // Clear draw
+    map.selectAll("*").remove();
+    map = d3.select("#treemap_svg");
+
+    // Format Data
     formatData();
     console.log(data);
 
+    // Give the data in cluster format
     const hierarchy = d3
       .hierarchy(data)
       .sum((d) => d.value) // Sum the value of each child
-      .sort((a, b) => b.value - a.value); // Sort by descening order
+      .sort((a, b) => b.value - a.value); // Sort by desending order
 
+    // Treemap Color
     const treeColor = d3.scaleOrdinal(d3.schemeCategory10);
 
-    const treemapLayout = d3.treemap().size([width, height]);
+    // Layout for treemap
+    const treemapLayout = d3
+      .treemap()
+      .size([width, height])
+      .padding(2)
+      .round(true);
 
+    // Apply layout to each element in the data
     treemapLayout(hierarchy);
 
+    // Create each leaf node
     const leaf = map
       .selectAll("g")
       .data(hierarchy.leaves())
       .join("g")
       .attr("transform", (d) => `translate(${d.x0}, ${d.y0})`);
 
+    // Style leaves
     leaf
       .append("rect")
       .attr("width", (d) => d.x1 - d.x0)
       .attr("height", (d) => d.y1 - d.y0)
-      .attr("fill", (d) => treeColor(d.data.name));
+      .attr("fill", (d) => treeColor(d.data.name))
+      .attr("stroke", "black")
+      .attr("stroke-width", 0.5)
+      .attr("rx", 1)
+      .attr("ry", 1)
+      .attr("opacity", 0.9);
   }
 
   // Format the data for the treemap in a hierarchal structure
@@ -115,7 +142,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Add an event listener to the button
   document.getElementById("save").addEventListener("click", function () {
-    console.log("Saved text");
-    saveText();
+    //Check for valid input
+    if (document.getElementById("wordbox").value == "") {
+      alert(`Text area is blank, please enter something 😊`);
+    } else {
+      saveText();
+    }
   });
 });
