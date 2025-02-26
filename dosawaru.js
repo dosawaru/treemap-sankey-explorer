6c1bfff6f886ad9b3f175bd724771fe35364d340
Daniel Osawaru <35234154+dosawaru@users.noreply.github.com>
2025-02-26 14:09:52 -0700

    Reformat character data object and implement d3 treemap

--- dosawaru.js
+++ dosawaru.js
@@ -1,7 +1,17 @@
+// Define margin and dimension for treemap
+
+const margin = 10;
+
+const width = 580 - margin;
+const height = 400 - margin;
+
+let data = {};
+
 document.addEventListener("DOMContentLoaded", function () {
   // Store the count for each letter/punctutaion
-  let letterCount = { consonants: {}, vowels: {}, punctuation: {} };
+  let charCount = { consonants: {}, vowels: {}, punctuation: {} };
 
+  // Create treemap
   const map = d3.select("#treemap_svg");
 
   function saveText() {
@@ -24,17 +34,17 @@ document.addEventListener("DOMContentLoaded", function () {
       // Add the character to the object and increment
       // Count the number of punctuation, consonants, and vowels
       if (consonants.includes(c)) {
-        letterCount.consonants[c] = (letterCount.consonants[c] || 0) + 1;
+        charCount.consonants[c] = (charCount.consonants[c] || 0) + 1;
         consonantsCount++;
       } else if (vowels.includes(c)) {
-        letterCount.vowels[c] = (letterCount.vowels[c] || 0) + 1;
+        charCount.vowels[c] = (charCount.vowels[c] || 0) + 1;
         vowelsCount++;
       } else if (punctuation.includes(c)) {
-        letterCount.punctuation[c] = (letterCount.punctuation[c] || 0) + 1;
+        charCount.punctuation[c] = (charCount.punctuation[c] || 0) + 1;
         punctuationCount++;
       }
     }
-    console.log(letterCount);
+    console.log(charCount);
     console.log(consonantsCount);
     console.log(vowelsCount);
     console.log(punctuationCount);
@@ -43,11 +53,69 @@ document.addEventListener("DOMContentLoaded", function () {
     drawTreemap();
   }
 
-  function drawTreemap() {}
+  function drawTreemap() {
+    formatData();
+    console.log(data);
+
+    const hierarchy = d3
+      .hierarchy(data)
+      .sum((d) => d.value) // Sum the value of each child
+      .sort((a, b) => b.value - a.value); // Sort by descening order
+
+    const treeColor = d3.scaleOrdinal(d3.schemeCategory10);
+
+    const treemapLayout = d3.treemap().size([width, height]);
+
+    treemapLayout(hierarchy);
+
+    const leaf = map
+      .selectAll("g")
+      .data(hierarchy.leaves())
+      .join("g")
+      .attr("transform", (d) => `translate(${d.x0}, ${d.y0})`);
+
+    leaf
+      .append("rect")
+      .attr("width", (d) => d.x1 - d.x0)
+      .attr("height", (d) => d.y1 - d.y0)
+      .attr("fill", (d) => treeColor(d.data.name));
+  }
+
+  // Format the data for the treemap in a hierarchal structure
+  // For each punctuation, consonants, and vowels, it is categorized in a childern node and a list of objects to created for each character(key) and value
+  // This helps with styling and access data for d3 treemap
+  function formatData() {
+    data = {
+      name: "characters",
+      children: [
+        {
+          name: "consonants",
+          children: Object.keys(charCount.consonants).map((key) => ({
+            char: key,
+            value: charCount.consonants[key],
+          })),
+        },
+        {
+          name: "vowels",
+          children: Object.keys(charCount.vowels).map((key) => ({
+            char: key,
+            value: charCount.vowels[key],
+          })),
+        },
+        {
+          name: "punctuation",
+          children: Object.keys(charCount.punctuation).map((key) => ({
+            char: key,
+            value: charCount.punctuation[key],
+          })),
+        },
+      ],
+    };
+  }
 
   // Add an event listener to the button
   document.getElementById("save").addEventListener("click", function () {
-    console.log("saved text");
+    console.log("Saved text");
     saveText();
   });
 });
